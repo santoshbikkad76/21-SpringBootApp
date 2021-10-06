24aa53b3a21b7681cdfdf06417f8f27162b37372
GanRaaJ <you@example.com>
2021-10-06 18:21:39 +0530

    IES-333: Santosh made some changes

--- src/main/java/com/bikkadIt/beans/Car.java
+++ src/main/java/com/bikkadIt/beans/Car.java
@@ -2,8 +2,15 @@ package com.bikkadIt.beans;
 
 public class Car {
 
-	public void car() {
+	public void start() {
 		//car class IES-122 added
 		
 	}
+	
+	public void run() {
+		
+		// IES -123
+	}
+	
+	//Santosh made some changes in car
 }
