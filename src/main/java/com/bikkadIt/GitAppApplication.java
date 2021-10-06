3ac1ab5ab2854df421acb84dac1a310bee5ef9a7
GanRaaJ <you@example.com>
2021-10-06 17:46:15 +0530

    main method modified again

--- src/main/java/com/bikkadIt/GitAppApplication.java
+++ src/main/java/com/bikkadIt/GitAppApplication.java
@@ -9,6 +9,8 @@ public class GitAppApplication {
 
 	public static void main(String[] args) {
 	ConfigurableApplicationContext context=	SpringApplication.run(GitAppApplication.class, args);
+	
+	// IES-134 changes done
 	context.close();
 	
 	}
