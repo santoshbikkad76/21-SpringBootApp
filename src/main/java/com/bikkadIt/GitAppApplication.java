d6b7844556f6419e5d3bd2b7c7c6598d1328091b
GanRaaJ <you@example.com>
2021-10-06 15:57:07 +0530

    main class modified

--- src/main/java/com/bikkadIt/GitAppApplication.java
+++ src/main/java/com/bikkadIt/GitAppApplication.java
@@ -2,12 +2,15 @@ package com.bikkadIt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GitAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitAppApplication.class, args);
+	ConfigurableApplicationContext context=	SpringApplication.run(GitAppApplication.class, args);
+	context.close();
+	
 	}
 
 }
